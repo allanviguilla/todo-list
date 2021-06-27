73027cff303acd28d80440b7fd68a73c7ebe02be
allanviguilla <viguilla.allan@gmail.com>
2021-06-27 01:03:20 -0700

    handle state, props, and input from form component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 //Import Components
 import Form from './components/form';
 import ToDoList from './components/todolist';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Allan's To-Do List</h1>
       </header>
-      <Form />
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
       <ToDoList />
     </div>
   );
